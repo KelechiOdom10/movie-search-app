be9648ddd8d9b869e1301cb97047bd8a7f2e8242
KelechiOdom10 <kelechiodom10@gmail.com>
2020-04-13 20:48:58 +0100

    Mapped the results of most popular movies into a card component from semantic ui

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
+import { Card, Icon, Image } from "semantic-ui-react";
+
+const API_KEY = "844dba0bfd8f3a4f3799f6130ef9e335";
+const API_URL = "https://api.themoviedb.org/3/";
+const IMAGE_URL = "https://image.tmdb.org/t/p/";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [popularMovies, setPopularMovies] = React.useState([]);
+
+	useEffect(() => {
+		fetch(`${API_URL}movie/popular?api_key=${API_KEY}&language=en-UK&page=1`)
+			.then(response => response.json())
+			.then(response => {
+				console.log(response);
+				setPopularMovies(response.results);
+			});
+	}, []);
+
+	const movies = popularMovies.map(result => {
+		return (
+			<Card>
+				<Image
+					src={`${IMAGE_URL}w300${result.poster_path}`}
+					wrapped
+					ui={false}
+					size="small"
+					fluid
+				/>
+				<Card.Content>
+					<Card.Header>{result.original_title}</Card.Header>
+					<Card.Meta>
+						<span className="date">{result.release_date}</span>
+					</Card.Meta>
+					{/* <Card.Description>{result.overview}</Card.Description> */}
+				</Card.Content>
+				<Card.Content extra>
+					<a href="">
+						<Icon name="star" />
+						{result.vote_average}/10
+					</a>
+				</Card.Content>
+			</Card>
+		);
+	});
+
+	return <div className="App">{movies}</div>;
 }
 
 export default App;
