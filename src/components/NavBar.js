24747763cb37ac1941a3cb0dc88d8c70d3eb0c70
RadhikaChamdal <radhika_chamdal@hotmail.co.uk>
2020-04-14 17:31:10 +0100

    used link to, to link to the pages

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,6 +4,9 @@ import {Link} from "@reach/router";
 function NavBar(){
     return (
         <div className = "Nav">
+            <Link to = "/"><li>Home</li></Link>
+            <Link to = "About"><li>About</li> </Link>
+           
 
         </div>
     )
