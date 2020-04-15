9de16ffe84608b5680a1f9e1b62be25d31dc4d80
RadhikaChamdal <radhika_chamdal@hotmail.co.uk>
2020-04-15 10:31:14 +0100

    created assets folder, changed link to/s, added default svg to nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,11 +3,30 @@ import {Link} from "@reach/router";
 
 function NavBar(){
     return (
-        <div className = "Nav">
-            <Link to = "/"><li>Home</li></Link>
-            <Link to = "About"><li>About</li> </Link>
-           
+        <div className="nav">
+			<Link to="/" style={{ color: "white" }}>
+				<img src="./assets/default.svg" alt="" style={{ height: "6vh" }} />
+				
+			</Link>
+			<ul className="nav-links">
+				<Link to="/" style={{ color: "white" }}>
+					<li>Home</li>
+				</Link>
+				<Link to="about" style={{ color: "white" }}>
+					<li className="li-1">About</li>
+				</Link>
+				<Link to="contact" style={{ color: "white" }}>
+					<li>Contact</li>
+				</Link>
+			</ul>
+		</div>
 
-        </div>
-    )
+
+
+
+
+
+
+
+    )     
 }
