c17779013615c5cdb934a936f68a38d79b0c9b5d
KelechiOdom10 <kelechiodom10@gmail.com>
2020-04-16 14:09:40 +0100

    Added the Movie Detail Page component to the Router in the App.js file

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from "./components/About";
 import { Router } from "@reach/router";
 import NavBar from "./components/NavBar";
 import "./App.css";
+import MovieDetailPage from "./components/MovieDetailPage";
 
 function App() {
 	return (
@@ -14,6 +15,7 @@ function App() {
 				<Home path="/" />
 				<Contact path="contact" />
 				<About path="about" />
+				<MovieDetailPage path="movies/:movieId" />
 			</Router>
 		</div>
 	);
