303a612b3594c96b18598d9c20a2bc2a358e2261
KelechiOdom10 <kelechiodom10@gmail.com>
2020-04-16 14:07:52 +0100

    Added the Movie App Info to the Movie Detail Page

--- src/components/MovieDetailPage.js
+++ src/components/MovieDetailPage.js
@@ -1,21 +1,41 @@
 import React, { useEffect } from "react";
+import { Icon } from "semantic-ui-react";
+import MovieAppInfo from "./MovieAppInfo";
 
 const API_KEY = "844dba0bfd8f3a4f3799f6130ef9e335";
 const API_URL = "https://api.themoviedb.org/3/";
 const IMAGE_URL = "https://image.tmdb.org/t/p/";
 
 function MovieDetailPage(props) {
-	const [movie, setMovie] = React.useState([]);
+	const [movieDetails, setMovieDetails] = React.useState([]);
 
 	useEffect(() => {
 		fetch(`${API_URL}movie/${props.movieId}?api_key=${API_KEY}&language=en-UK&`)
 			.then(response => response.json())
 			.then(response => {
-				setMovie(response);
+				setMovieDetails([response]);
 				console.log(response);
 			});
 	}, []);
 
-	return <div className="App"></div>;
+	console.log("Logging out movie details", movieDetails);
+
+	const movieDetail = movieDetails.map(result => {
+		return (
+			<MovieAppInfo
+				background={`${IMAGE_URL}w1280${result.backdrop_path}`}
+				poster={`${IMAGE_URL}w342${result.poster_path}`}
+				title={result.original_title}
+				tagline={result.tagline}
+				overview={result.overview}
+				runtime={result.runtime}
+				budget={result.budget}
+				revenue={result.revenue}
+				rating={result.vote_average}
+			/>
+		);
+	});
+
+	return <div>{movieDetail}</div>;
 }
 export default MovieDetailPage;
