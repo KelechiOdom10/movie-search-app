227fc88f7ff539610adfc76669cb501d9fd526db
KelechiOdom10 <kelechiodom10@gmail.com>
2020-04-16 19:51:27 +0100

    Added a key to the Movie App Info component and added commas to the revenue and budget being mapped

--- src/components/MovieDetailPage.js
+++ src/components/MovieDetailPage.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import { Icon } from "semantic-ui-react";
 import MovieAppInfo from "./MovieAppInfo";
 
 const API_KEY = "844dba0bfd8f3a4f3799f6130ef9e335";
@@ -8,29 +7,35 @@ const IMAGE_URL = "https://image.tmdb.org/t/p/";
 
 function MovieDetailPage(props) {
 	const [movieDetails, setMovieDetails] = React.useState([]);
+	const movie_id = `${props.movieId}`;
 
 	useEffect(() => {
-		fetch(`${API_URL}movie/${props.movieId}?api_key=${API_KEY}&language=en-UK&`)
+		fetch(`${API_URL}movie/${movie_id}?api_key=${API_KEY}&language=en-UK&`)
 			.then(response => response.json())
 			.then(response => {
 				setMovieDetails([response]);
-				console.log(response);
 			});
-	}, []);
+	});
 
-	console.log("Logging out movie details", movieDetails);
-
-	const movieDetail = movieDetails.map(result => {
+	const movieDetail = movieDetails.map((result, index) => {
+		const movieRevenue = result.revenue.toLocaleString(navigator.language, {
+			minimumFractionDigits: 0,
+		});
+		const movieBudget = result.budget.toLocaleString(navigator.language, {
+			minimumFractionDigits: 0,
+		});
 		return (
 			<MovieAppInfo
+				key={index}
 				background={`${IMAGE_URL}w1280${result.backdrop_path}`}
 				poster={`${IMAGE_URL}w342${result.poster_path}`}
 				title={result.original_title}
 				tagline={result.tagline}
 				overview={result.overview}
+				date={result.release_date}
 				runtime={result.runtime}
-				budget={result.budget}
-				revenue={result.revenue}
+				budget={movieBudget}
+				revenue={movieRevenue}
 				rating={result.vote_average}
 			/>
 		);
