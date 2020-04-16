b45d0699b45539b1046f5faea6f897111e670253
KelechiOdom10 <kelechiodom10@gmail.com>
2020-04-16 19:40:52 +0100

    Changed the structure of the Movie App info component to style it more comfortably

--- src/components/MovieAppInfo.js
+++ src/components/MovieAppInfo.js
@@ -6,27 +6,48 @@ function MovieAppInfo(props) {
 		<div
 			className="movieInfo"
 			style={{
-				backgroundImage: `linear-gradient(to right, rgba(106, 105, 120, 0.86) 150px, rgba(134, 132, 132, 0.79) 100%),
+				backgroundImage: `linear-gradient(to right, rgba(106, 105, 120, 0.86) 150px, rgba(77, 75, 75, 0.84) 100%),
                 url(${props.background})`,
 			}}
 		>
 			<div className="mainInfo">
-				<img
-					src={props.poster}
-					style={{
-						height: "35vh",
-						borderRadius: "5px",
-					}}
-				/>
-				<div>
-					<h3>{props.title}</h3>
-					<h5>{props.tagline}</h5>
-					<p>{props.overview}</p>
+				<img className="mainInfoImg" src={props.poster} alt="movie poster" />
+				<div className="info">
+					<h1>{props.title}</h1>
 					<p>{props.date}</p>
-					<p>{props.runtime} minutes</p>
-					<p>{props.budget}</p>
-					<p>{props.revenue}</p>
-					<Icon name="star">{props.rating}/10</Icon>
+					<p className="tagLine">{props.tagline}</p>
+					<h3>Overview</h3>
+					<h5 className="plot">{props.overview}</h5>
+
+					<div className="lastInfo">
+						<p>
+							<Icon name="clock outline" style={{ display: "inline" }}>
+								{" "}
+								Runtime: {props.runtime} minutes
+							</Icon>
+						</p>
+						<p>
+							<Icon name="money bill alternate" style={{ display: "inline" }}>
+								{" "}
+								Budget: ${props.budget}
+							</Icon>
+						</p>
+						<p>
+							<Icon
+								name="money bill alternate outline"
+								style={{ display: "inline" }}
+							>
+								{" "}
+								Revenue: ${props.revenue}
+							</Icon>
+						</p>
+						<p>
+							<Icon name="star" style={{ display: "inline" }}>
+								{" "}
+								{props.rating}/10
+							</Icon>
+						</p>
+					</div>
 				</div>
 			</div>
 		</div>
