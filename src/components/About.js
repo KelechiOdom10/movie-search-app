2c93e3974d4b64e8df57c835fa9fd16e665ed430
gerda-sataite <gerda.sataite@gmail.com>
2020-04-17 10:10:07 +0100

    started to add information to about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,25 @@
 import React from "react";
 
 function About() {
-	return <div>No</div>;
+	return <div className="aboutpage">
+		<h1>MOVIE SEARCH APP</h1>
+		<h2>Motivation</h2>
+		<p>
+
+		</p>
+		<h2>API Reference</h2>
+		<p>
+			<a 
+			href=""
+			target="_blank"
+			rel="noopener noreferrer"
+			>
+				The Movie Database API
+			</a>
+		</p>
+		No
+		
+		</div>;
 }
 
 export default About;
