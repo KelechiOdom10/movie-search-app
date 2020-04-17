d7feb8b7d23881e5dc7714f1f8f9149d5489ef19
KelechiOdom10 <kelechiodom10@gmail.com>
2020-04-17 19:42:47 +0100

    Added a fall back image path for the actor card component using an image path state

--- src/components/ActorCard.js
+++ src/components/ActorCard.js
@@ -1,9 +1,21 @@
 import React from "react";
+import picture from "../default.jpg";
 
 function ActorCard(props) {
+	const [imagePath, setImagePath] = React.useState(`${props.image}`);
+
+	const fallBackPath = () => {
+		setImagePath(picture);
+	};
+
 	return (
 		<div className="main-card">
-			<img className="image" src={props.image} />
+			<img
+				className="image"
+				src={imagePath}
+				alt="Actor images"
+				onError={fallBackPath}
+			/>
 			<div className="lower-card">
 				<h2>{props.actor}</h2>
 				<h5>{props.character}</h5>
